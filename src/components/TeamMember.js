8ba5ccdeab264464145c33d64933d9a9de2682a9
David Kvas <david@nodevision.com.au>
2018-11-13 13:49:25 +1100

    Team page

---
+++ src/components/TeamMember.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { withPrefix } from "gatsby";
+import "./TeamMember.sass";
+
+const TeamMember = ({ imgUrl, name, title, bio, linkedInUrl }) => {
+	return (
+		<div className="column team-member-box">
+			<div className="tm-img" style={{ backgroundImage: `url(${withPrefix("img/" + imgUrl)})` }} />
+			<div className="tm-name">{name}</div>
+			<div className="tm-title">{title}</div>
+			<div className="tm-bio">{bio}</div>
+			<a href={linkedInUrl} className="tm-li">
+				<img src={""} />
+			</a>
+		</div>
+	);
+};
+
+export default TeamMember;
---
+++ src/pages/team.js
@@ -0,0 +1,75 @@
+import React from "react";
+import { Link } from "gatsby";
+import "./index.sass";
+import Layout from "../components/layout";
+import "./team.sass";
+import TeamMember from "../components/TeamMember";
+
+const TeamPage = () => (
+	<Layout>
+		<div className="team-page">
+			<section className="section header-section">
+				<div className="container">
+					<h1 className="page-title">Meet the Team</h1>
+					<div className="page-subtitle">
+						A multi disciplinary team with proven ability to deliver complex solutions
+					</div>
+				</div>
+			</section>
+			<section className="section team-members">
+				<div className="container">
+					<div className="columns is-multiline">
+						<TeamMember
+							name="Kain Warwick"
+							imgUrl="team-kain.png"
+							title="Founder"
+							bio="Before launching Havven, Kain built the largest cryptocurrency payment platform in Australia, with tens of millions in transaction volume."
+						/>
+						<TeamMember
+							name="Kain Warwick"
+							imgUrl="team-kain.png"
+							title="Founder"
+							bio="Before launching Havven, Kain built the largest cryptocurrency payment platform in Australia, with tens of millions in transaction volume."
+						/>
+						<TeamMember
+							name="Kain Warwick"
+							imgUrl="team-kain.png"
+							title="Founder"
+							bio="Before launching Havven, Kain built the largest cryptocurrency payment platform in Australia, with tens of millions in transaction volume."
+						/>
+					</div>
+				</div>
+			</section>
+			<section className="section is-white join-team-section">
+				<div className="container">
+					<div className="section-title">Join the Team</div>
+					<div className="section-desc pb-70">
+						We are looking for highly skilled individuals to join the Havven team and help us
+						deliver a decentralised payment network and stablecoin to the world. If you're looking
+						for a challenging project and want to contribute to building a critical layer of the
+						blockchain ecosystem, then please get in touch!
+					</div>
+					<div className="section-title is-subtle pb-30">Open Positions</div>
+					<div className="columns open-positions pb-60">
+						<div className="column">
+							<div>Senior Blockchain Engineer</div>
+						</div>
+						<div className="column">
+							<div>Head of Marketing and Communication</div>
+						</div>
+						<div className="column">
+							<div>Creative Director</div>
+						</div>
+					</div>
+					<div className="section-desc join-team-desc">
+						Looking to join the team but don’t see a position suited to you? We’re always interested
+						in hearing from people who are interested in joining our mission. Say hello at{" "}
+						<a href="mailto:careers@havven.io">careers@havven.io</a>
+					</div>
+				</div>
+			</section>
+		</div>
+	</Layout>
+);
+
+export default TeamPage;
