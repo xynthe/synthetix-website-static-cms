0953a93056827fab76283c476370978bfa4f682c
David Kvas <david@nodevision.com.au>
2018-11-15 16:17:33 +1100

    Favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,7 +38,7 @@ module.exports = {
 				background_color: "#663399",
 				theme_color: "#663399",
 				display: "minimal-ui",
-				icon: "static/img/favicon.ico"
+				icon: "src/resources/gatsby-icon.png" // This path is relative to the root of the site.
 			}
 		},
 		"gatsby-plugin-offline",
