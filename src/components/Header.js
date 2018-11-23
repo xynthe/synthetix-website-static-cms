9e6b9996127c13233d6a07cafcb63b832ff9d26f
David Kvas <david@nodevision.com.au>
2018-11-23 12:28:08 +1100

    Header update

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,66 +3,6 @@ import { graphql, Link, StaticQuery } from "gatsby";
 import Logo from "../resources/logo-havven.svg";
 import cx from "classnames";
 
-let menu = [
-	{
-		name: "Foundation",
-		link: "",
-		isExt: false,
-		children: [
-			{ name: "Team", link: "/team", isExt: false },
-			{ name: "Partner with us", link: "/partner-with-us", isExt: false }
-		]
-	},
-	{
-		name: "Developers",
-		link: "",
-		isExt: false,
-		children: [
-			{ name: "Dev Portal", link: "https://developer.havven.io/api/docs/home.html", isExt: true },
-			{
-				name: "Havven-JS Lib",
-				link: "https://developer.havven.io/api/docs/home.html",
-				isExt: true
-			},
-			{
-				name: "Contracts",
-				link: "https://developer.havven.io/api/docs/deployed-contracts.html",
-				isExt: true
-			}
-		]
-	},
-	{
-		name: "Learn",
-		link: "",
-		isExt: false,
-		children: [
-			{ name: "How It Works ", link: "/how-it-works", isExt: false },
-			{ name: "Blog", link: "https://blog.havven.io/", isExt: true },
-			{ name: "Whitepaper", link: "https://havven.io/uploads/havven_whitepaper.pdf", isExt: true }
-		]
-	},
-	{
-		name: "Products",
-		link: "",
-		isExt: false,
-		children: [
-			{ name: "Dashboard", link: "https://dashboard.havven.io/", isExt: true },
-			{ name: "Mintr", link: "https://mintr.havven.io/", isExt: true },
-			{ name: "Swappr", link: "https://swappr.io/", isExt: true },
-			{ name: "eStore", link: "https://shop.havven.io/", isExt: true }
-		]
-	},
-	{
-		name: "Connect",
-		link: "",
-		isExt: false,
-		children: [
-			{ name: "Community", link: "/community", isExt: false },
-			{ name: "Contact Us", link: "/contact-us", isExt: false }
-		]
-	}
-];
-
 export default class Header extends React.Component {
 	state = {
 		isOpen: false
@@ -143,26 +83,6 @@ export default class Header extends React.Component {
 																{el2.title}
 															</UiLink>
 														))}
-												</div>
-											</div>
-										))}
-										{menu.map((el, idx) => (
-											<div className="dropdown is-hoverable" key={idx}>
-												<div className="dropdown-trigger">
-													<UiLink
-														className="navbar-item"
-														aria-haspopup="true"
-														aria-controls={el.name}
-													>
-														<span>{el.name}</span>
-													</UiLink>
-												</div>
-												<div className="dropdown-menu" id={el.name} role="menu">
-													{el.children.map((el2, idx2) => (
-														<UiLink key={idx2} to={el2.link} isExt={el2.isExt}>
-															{el2.name}
-														</UiLink>
-													))}
 												</div>
 											</div>
 										))}
