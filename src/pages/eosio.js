f6461927e0b9313ef1203fc9f9cb77f234c04b6a
David Kvas <david@nodevision.com.au>
2018-11-28 16:24:16 +1100

    Rebranding for Synthetix

--- src/pages/eosio.js
+++ src/pages/eosio.js
@@ -15,28 +15,28 @@ export default class IosIoPage extends React.Component {
 					<section className="section header-section">
 						<div className="container">
 							<img src={logo} alt="EOSIO" className="logo pb-70 pt-30" />
-							<h1 className="page-title">Havven is launching on EOSIO!</h1>
+							<h1 className="page-title">Synthetix is launching on EOS!</h1>
 							<div className="page-subtitle">
-								Havven is launching its payment network and stablecoin on EOSIO, and 50% of all
-								HAVeos tokens are being distributed to HAV holders on Ethereum!
+								Synthetix is launching its payment network and stablecoin on EOSIO, and 50% of all
+								FLUXeos tokens are being distributed to FLUX holders on Ethereum!
 							</div>
 						</div>
 					</section>
-					<section className="section is-white steps-section">
+					<section className="section is-white steps-section pt-80">
 						<div className="container">
-							<div className="st-1 pb-40">How to recieve HAVeos</div>
+							<div className="st-1 pb-40">How to recieve FLUXeos</div>
 							<div className="sd-1 section-desc pb-50">
-								The HAVeos Distribution will reward HAV(eth) holders for supporting the project,
-								allowing them to help enable stable payments on the EOSIO blockchain. In the Havven
-								payment network, HAV tokens are the collateral token that back nUSD, the stablecoin.
-								Over $1.5m nUSD has already been minted by HAVeth holders!
+								The FLUXeos Distribution will reward FLUX(eth) holders for supporting the project,
+								allowing them to help enable stable payments on the EOSIO blockchain. In the
+								Synthetix payment network, FLUX tokens are the collateral token that back sUSD, the
+								stablecoin. Over $1.5m sUSD has already been minted by FLUXeth holders!
 							</div>
 						</div>
 						<div className="st-2 pb-30">How it Works</div>
 						<div className="content sl-1 pb-80">
 							<ul>
 								<li>
-									50m HAVeos will be distributed between all HAVeth holders that hold at least 1000
+									50m FLUXeos will be distributed between all HAVeth holders that hold at least 1000
 									HAVeth.
 								</li>
 								<li>
@@ -48,12 +48,12 @@ export default class IosIoPage extends React.Component {
 								<li>Exchange wallets are currently not supported.</li>
 								<li>
 									After the second snapshot, we will advertise a dApp through which participants
-									have six months to submit an EOS wallet address to receive their HAVeos in.
+									have six months to submit an EOS wallet address to receive their FLUXeos in.
 								</li>
 							</ul>
 						</div>
 						<div className="st-3 pb-50">
-							If you wish to purchase HAVeth to receive HAVeos, you can do so at the following
+							If you wish to purchase FLUXeth to receive FLUXeos, you can do so at the following
 							exchanges
 						</div>
 						<div className="columns exchanges pb-80">
@@ -85,7 +85,7 @@ export default class IosIoPage extends React.Component {
 							</a>
 						</div>
 						<div className="sd-2 pb-90">
-							For more details on the HAVeos Distribution, please visit our{" "}
+							For more details on the FLUXeos Distribution, please visit our{" "}
 							<a
 								href="https://blog.havven.io/haveos-distribution-eligibility-snapshot-dates-and-more-57d928b12c8a"
 								target="_blank"
