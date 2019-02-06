db5b7d385136bacda22e479233538fc840bda265
garthtravers <39815229+garthtravers@users.noreply.github.com>
2019-02-06 11:21:33 +1100

    Update index.js
    Removed references to formerly being Havven

--- src/pages/index.js
+++ src/pages/index.js
@@ -122,13 +122,13 @@ class IndexPage extends React.Component {
 								<UiImg src={"/assets/synthetix-title.png"} alt="Synthetix" isResponsive />
 							</h1>
 							<div className="page-subtitle">
-								Synthetix, formerly Havven, is a cryptoasset-backed network that enables the creation
+								Synthetix is a cryptoasset-backed network that enables the creation
 								of on-chain synthetic assets.
 							</div>
 							<div className="has-text-centered pb-10">
-								<a href="https://blog.havven.io/havven-is-transforming-into-synthetix-2fdf727b8892" className="is-button is-announcement">
+								<a href="https://synthetix.exchange" className="is-button is-announcement">
 									<span>NEW</span>
-									<span>Havven is now Synthetix. Learn more about our rebrand here!</span>
+									<span>Convert between different Synths on Synthetix.Exchange!</span>
 								</a>
 							</div>
 						</div>
