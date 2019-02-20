4dba01b319a15c29d7a452b4a4ea710141ae0686
Sergey Vassilyev <commandovmk@gmail.com>
2019-02-20 18:33:53 +1100

    exchange page faqs section

--- src/pages/products/exchange.js
+++ src/pages/products/exchange.js
@@ -5,6 +5,7 @@ import Layout from "../../components/layout";
 import "./exchange.sass";
 import exchangeLogo from "../../resources/products/exchange.svg";
 import exchangeButton from "../../resources/products/exchange-button.svg";
+import joinDiscordButton from "../../resources/products/join-discord.svg";
 
 const ExchangePage = (props) => {
 	const { synthetixExchangeUrl, discordUrl } = props.data.site.siteMetadata;
@@ -24,7 +25,8 @@ const ExchangePage = (props) => {
 					<div className="container">
 						<div className="section-title sp7 pt-10">Synthetix.Exchange</div>
 						<div className="section-desc sp1">
-							Synthetix.Exchange provides an intuitive interface for conversions between the full range of Synths available (including sUSD, sEUR, sAUD, sJPY, sKRW, and sXAU). It provides access to a variety of synthetic forms of fiat currencies and gold, as well opening up forex trading possibilities. 						</div>
+							Synthetix.Exchange provides an intuitive interface for conversions between the full range of Synths available. It provides access to a variety of synthetic forms of fiat currencies, cryptocurrencies, and commodities.
+						</div>
 						<div className="exchange-button"><a href={synthetixExchangeUrl} target="_blank"><img src={exchangeButton} /></a></div>
 					</div>
 				</section>
@@ -36,7 +38,7 @@ const ExchangePage = (props) => {
 								<div className="section-desc sp6 pb-40">
 									You can use Synthetix.Exchange with MetaMask, Trezor, or Ledger wallets. Simply connect a wallet that holds at least one Synth, and your balance will be displayed, along with the conversion rates.
  									<br/><br/>
-									Converting is as simple as choosing a Synth to convert from and to convert to, and confirming the transaction. Once it has been processed on the blockchain, you’re all done! All conversions are on-chain, and thus generate the standard 0.15% fee for all Synth transactions that are distributed to locked SNX holders.
+									Converting is as simple as choosing a Synth to convert from and to convert to, and confirming the transaction. Once it has been processed on the blockchain, you’re all done! All conversions are carried out on-chain.
 									<br/><br/>
 									If you have any more questions, please come join our community on <a href={discordUrl} target="_blank">Discord</a>. 
 								</div>
@@ -46,7 +48,63 @@ const ExchangePage = (props) => {
 							</div>
 						</div>
 					</div>
-				</section>	
+				</section>
+				<section className="section is-white exchange-faq">
+					<div className="container">
+						<div className="section-title sp5">FAQs</div>
+						<div className="columns">
+							<div className="column is-half first-column">
+								<p> 
+									<p class="subtitle">What is a synthetic asset?</p>
+									A synthetic asset is an asset or mixture of assets that has/have the same value or effect as another asset. Within the Synthetix network, synthetic assets (Synths) track the value of underlying assets and allow exposure to an asset (e.g. gold) without the requirement of actually holding it.
+								</p>
+								<br/>
+								<p> 
+									<p class="subtitle">Does it cost anything to make a trade?</p>
+									Apart from ETH as gas, there is a fee for all Synth exchanges. It is currently 0.15%, but may be adjusted in the future according to demand. These exchange fees are distributed to SNX holders, who are providing collateral as backing for all Synths.
+								</p>
+								<br/>
+								<p> 
+									<p class="subtitle">Do I need to hold SNX to use Synthetix.Exchange?</p>
+									No, you only require ETH and Synths.
+								</p>
+								<br/>
+								<p> 
+									<p class="subtitle">What happens if the price changes just as I make a transaction?</p>
+									The prices you see at Synthetix.Exchange are the prices at that time. when you make a transaction the new price feed is provided by our Oracle and the amount you get might be slightly different.
+								</p>
+								<br/>
+								<p> 
+									<p class="subtitle">Is there a price or transaction limit?</p>
+									No. Since there is no counter party at Synthetix.Exchange, you are only limited by your wallet balance. 
+								</p>
+							</div>
+							<div class="column is-half second-column">
+								<p> 
+									<p class="subtitle">What do I need to use Synthetix.Exchange?</p>
+									You’ll need a compatible ERC20 wallet (currently MetaMask, Ledger and Trezor wallets). Secondly, this wallet must contain ETH for gas. Thirdly, for you to make a trade your wallet must be holding at least 0.01 of a Synth. If it doesn’t hold Synths, you can purchase sUSD through Synthetix.Exchange with ETH. 
+								</p>
+								<br/>
+								<p> 
+									<p class="subtitle">When I trade from one Synth into another, where are these tokens coming from?</p>
+									There is no counterparty in Synthetix.Exchange, which means that you essentially converts one Synth into another one, with the values of each Synth in a trade being taken from exchange rates at the time of the transaction.
+								</p>
+								<br/>
+								<p> 
+									<p class="subtitle">Where does Synthetix Exchange get the prices from?</p>
+									Synthetix.Exchange uses a private oracle that pulls feeds from multiple credible financial market resources. We are currently looking into options for decentralised oracles.
+								</p>
+								<br/>
+								<p> 
+									<p class="subtitle">How often do you update the prices?</p>
+									The oracle updates prices every 3 minutes. 
+								</p>										
+								<br/>
+								<a class="discord-button" href={discordUrl} target="_blank"><img src={joinDiscordButton} /></a>
+							</div>
+						</div>
+					</div>
+				</section>
 			</div>
 		</Layout>
 )};
